97ccfa5052de546332e8556283e585dececdcf67
Ethan Jacob Marx <ethan.marx@ligo.org>
2024-01-08 16:01:31 -0800

    add args to triton dep

--- kubeml/resources/triton/deployment.py
+++ kubeml/resources/triton/deployment.py
@@ -5,10 +5,17 @@ from .templates import servers
 
 
 class TritonServerDeployment(Deployment):
-    def __init__(self, *args, command: List[str], **kwargs):
+    def __init__(
+        self, *args, command: List[str], command_args: List[str], **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self.command = command
         self.set_command()
+
+    def set_args(self):
+        self["spec"]["template"]["spec"]["containers"][0][
+            "args"
+        ] = self.command_args
 
     def set_command(self):
         self["spec"]["template"]["spec"]["containers"][0][
